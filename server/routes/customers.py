15866258e9ef5de7206fa2e3c8d7425f7bf8cdb7
Jacob Mowat <zaxquit@protonmail.com>
2023-08-30 11:58:43 +0100

    Added anaother route to get a specific customer by id

--- server/routes/customers.py
+++ server/routes/customers.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, status, Path
 from typing import List
 from data.database import ActivePrismaClient
-from schemas import schemas
+from schemas.customer import Customer
 from services import customers as service
 
 # Create our API router
@@ -17,9 +17,21 @@ async def get_db_session():
 
 @router.get(
     "/",
-    response_model=List[schemas.Customer],
+    response_model=List[Customer],
     summary="Get all customers"
 )
 def get_all_customers(db: ActivePrismaClient = Depends(get_db_session)):
-    return service.get_all_customers(db)
+    return service.retrieve_all_customers(db)
+
+
+@router.get(
+    "/{customer_id}",
+    response_model=Customer,
+    summary="Get a customer by ID"
+)
+def get_customer_by_id(
+    customer_id: int = Path(..., gt=0),
+    db: ActivePrismaClient = Depends(get_db_session)
+):
+    return service.retrieve_customer_by_id(db, customer_id)
 
