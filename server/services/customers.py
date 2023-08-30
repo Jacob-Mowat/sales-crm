ca105f80e225d1ef9954f808826f6fde566df4f8
Jacob Mowat <zaxquit@protonmail.com>
2023-08-30 11:59:39 +0100

    Added new service

--- server/services/customers.py
+++ server/services/customers.py
@@ -2,3 +2,6 @@ from prisma.models import Customer
 
 def retrieve_all_customers(db):
     return db.customer.find_many()
+
+def retrieve_customer_by_id(db, customer_id: int):
+    return db.customer.find_first(where={"id": customer_id})
