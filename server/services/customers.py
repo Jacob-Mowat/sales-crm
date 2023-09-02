359c111078b150962393a4bfa400bec8de455b33
Jacob Mowat <zaxquit@protonmail.com>
2023-09-03 00:12:22 +0100

    [feat] Implementation of create-contact endpoint started.

--- server/services/customers.py
+++ server/services/customers.py
@@ -1,5 +1,5 @@
 from data.database import prisma
-from schemas.customer import CreateCustomer, Customer
+from schemas.customer import CreateCustomer, Customer, CreateCustomerContact, CustomerContact
 from typing import Union
 
 async def retrieve_all_customers():
@@ -15,6 +15,16 @@ async def retrieve_customer_by_id(customer_id: int):
 async def create_customer(customer: CreateCustomer) -> Union[Customer, None]:
     try :
         data: Customer = await prisma.customer.create(customer.__dict__)
+        return data
+    except Exception as e:
+        print(e)
+        return None
+    
+async def create_customer_contact(customer_id: int, customer_contact: CreateCustomerContact) -> Union[CustomerContact, None]:
+    try :
+        _cc = customer_contact.__dict__
+        _cc["customerId"] = customer_id 
+        data: CustomerContact = await prisma.customercontact.create(_cc)
         return data
     except Exception as e:
         print(e)
