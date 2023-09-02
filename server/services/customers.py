51cc25827ad93a4015d6b500e2a4edd3e73374e4
Jacob Mowat <zaxquit@protonmail.com>
2023-09-02 04:20:52 +0100

    [fix] Added try/catch around find_first for Customer.

--- server/services/customers.py
+++ server/services/customers.py
@@ -6,8 +6,12 @@ async def retrieve_all_customers():
     return await prisma.customer.find_many()
 
 async def retrieve_customer_by_id(customer_id: int):
-    return await prisma.customer.find_first(where={"id": customer_id})
-
+    try:
+        return await prisma.customer.find_first(where={"id": customer_id})
+    except Exception as e:
+        print(e)
+        return None
+    
 async def create_customer(customer: CreateCustomer) -> Union[Customer, None]:
     try :
         data: Customer = await prisma.customer.create(customer.__dict__)
